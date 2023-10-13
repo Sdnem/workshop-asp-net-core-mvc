4177a1813839208269256ccf621553adab81e9bf
Pedro Mendonça <pedroh36mendonca@gmail.com>
2023-10-13 16:49:37 -0300

    Simple Create form

--- SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -10,6 +10,7 @@ namespace SalesWebMvc.Services
     public class SellerService
     {
         private readonly SalesWebMvcContext _context;
+
         public SellerService(SalesWebMvcContext context)
         {
             _context = context;
@@ -18,6 +19,12 @@ namespace SalesWebMvc.Services
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
